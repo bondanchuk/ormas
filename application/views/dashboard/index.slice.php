4d55318551eb2d72ca0d745b1b19a615e4f916fd
bch <bondanchorisma.umrah@gmail.com>
2020-02-12 13:45:17 +0700

    update dashboard dan register

--- application/views/dashboard/index.slice.php
+++ application/views/dashboard/index.slice.php
@@ -12,7 +12,7 @@
                     </div>
                     <div class="ibox-content">
                         <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover dataTables-example" >
+                    <table id="table_dashboard" class="table table-striped table-bordered table-hover dataTables-example" >
                     <thead>
                     <tr>
                         <th width="2%">No.</th>
@@ -20,7 +20,6 @@
                         <th>Nama Ormas</th>
                         <th>Alamat Kantor</th>
                         <th>Cabang</th>
-                        <th width="5%">Status</th>
                         <th width="5%">Aksi</th>
                     </tr>
                     </thead>
@@ -40,7 +39,7 @@
   <!-- Page-Level Scripts -->
   <script>
         $(document).ready(function(){
-            $('.dataTables-example').DataTable({
+            $('#table_dashboard').DataTable({
                 pageLength: 25,
                 responsive: true,
 				"processing":true,
@@ -67,9 +66,29 @@
                     }
                     }
                 ]
-
             });
 
+
+			$('#table_dashboard').on('click','.lihat_record',function(){
+				var id_pejabat = $(this).data('kode');
+				$.ajax({
+					url:"{{base_url('Dashboard/get_detail')}}",
+					method:"POST",
+					data:{id_pejabat:id_pejabat},
+					dataType:"json",
+					success: function(data){
+						$('#modal-ubah').modal('show');
+						$('#ubah-id-pejabat').val(data.ubah_id_pejabat);
+						$('#ubah-jabatan').val(data.ubah_jabatan);
+						$('#ubah-nama').val(data.ubah_nama);
+						$('#ubah-pangkat').val(data.ubah_pangkat);
+						$('#ubah-nip').val(data.ubah_nip);
+						$('#ubah-dinas').val(data.ubah_dinas);
+						$('#ubah-provinsi').val(data.ubah_provinsi);
+						$('#ubah-status').val(data.ubah_status).trigger("change");
+					}
+				})
+			});
         });
 
     </script>
