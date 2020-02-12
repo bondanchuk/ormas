25c9594bee381dd2a0a1f2587e0b61fffa3aec6a
bch <bondanchorisma.umrah@gmail.com>
2020-02-12 14:53:19 +0700

    fix dashboard -final

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -40,17 +40,30 @@ class Dashboard extends CI_Controller
 	function get_detail()
 	{
 		$output = array();
-		$data = $this->model_dashboard->get_detail_model($this->input->post('id_pejabat'));
+		$data = $this->model_dashboard->get_detail_model($this->input->post('id_ormas'));
 		foreach($data as $row)
 		{
-			$output['ubah_id_pejabat'] = $row->id_ormas;
-			$output['ubah_jabatan'] = $row->Jabatan;
-			$output['ubah_nama'] = $row->Nama;
-			$output['ubah_pangkat'] = $row->Pangkat;
-			$output['ubah_nip'] = $row->NIP;
-			$output['ubah_dinas'] = $row->Dinas;
-			$output['ubah_provinsi'] = $row->Provinsi;
-			$output['ubah_status'] = $row->Status;
+			$output['lihat_id_ormas'] = $row->id_ormas;
+			$output['lihat_tanggal_daftar'] = $row->tanggal_daftar;
+			$output['lihat_nama_ormas'] = $row->nama_ormas;
+			$output['lihat_nama_notaris'] = $row->nama_notaris;
+			$output['lihat_nomortglakta'] = $row->nomortglakta;
+			$output['lihat_bidang'] = $row->bidang;
+			$output['lihat_program_kerja'] = $row->program_kerja;
+			$output['lihat_alamat_kantor'] = $row->alamat_kantor;
+			$output['lihat_asas_ciri_ormas'] = $row->asas_ciri_ormas;
+			$output['lihat_tujuan_ormas'] = $row->tujuan_ormas;
+			$output['lihat_nama_pendiri'] = $row->nama_pendiri;
+			$output['lihat_agama_pendiri'] = $row->agama_pendiri;
+			$output['lihat_kewarganegaraan_pendiri'] = $row->kewarganegaraan_pendiri;
+			$output['lihat_nama_ketua'] = $row->nama_ketua;
+			$output['lihat_agama_ketua'] = $row->agama_ketua;
+			$output['lihat_kewarganegaraan_ketua'] = $row->kewarganegaraan_ketua;
+			$output['lihat_nama_sekretaris'] = $row->nama_sekretaris;
+			$output['lihat_nama_bendahara'] = $row->nama_bendahara;
+			$output['lihat_cabang'] = $row->cabang;
+			$output['lihat_sumber_keuangan'] = $row->sumber_keuangan;
+			$output['lihat_berbadan_hukum'] = $row->berbadan_hukum;
 		}
 		echo json_encode($output);
 	}
